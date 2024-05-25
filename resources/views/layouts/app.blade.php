a19de1366f93a6b874deb9a6df59acc9daa86494
micha <msawicki@student.agh.edu.pl>
2024-05-25 19:54:58 +0200

    Adding footer and setting style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    @vite(['resources/css/home.css', 'resources/js/app.js'])
+    @vite(['resources/css/home.css','resources/css/footer.css', 'resources/js/app.js'])
 </head>
 <body>
 @include('components.header')
