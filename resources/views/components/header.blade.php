247d6ba75bb243cbc2b58ae7c6860956a9ff85c4
micha <msawicki@student.agh.edu.pl>
2024-06-01 17:03:04 +0200

    New Logo in header and some changes in footer

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header class="site-header sticky-top py-1">
     <nav class="container d-flex flex-column flex-md-row justify-content-between">
         <a class="py-2" href="#" aria-label="Product">
-{{--            icon--}}
+            <img src="{{ Vite::asset('resources/pictures/logo_napis_white.png') }}" alt="LogoHeader" class="img-fluid mb-1" style="max-width: 250px;">
         </a>
         <a class="py-2 d-none d-md-inline-block" href="{{ route('home') }}">Strona główna</a>
         <a class="py-2 d-none d-md-inline-block" href="{{ route('room') }}">Sale do wynajęcia</a>
